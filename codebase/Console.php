ec6d2c3932c35b38eeec7cd86ea0c63d0875c33e
emyassine <info@numerimondes.com>
2026-05-21 05:00:12 +0100

    WIP

---
+++ codebase/Console.php
@@ -0,0 +1,85 @@
+<?php declare(strict_types=1);
+namespace PhpCli;
+
+/** Good to know: PHP has a $argv built-in variable
+ *  It is a predefined variable that contains an array of all the arguments
+ *  passed to a script when it is executed from the command line (CLI).  */
+ if (!PHP_SAPI === "cli") { \die("This script must be run from the command line."); }
+
+// foreach (glob(__DIR__ . "/traits/*.php", GLOB_NOSORT) as $filename) {
+//     echo "$filename\n";
+// }
+
+$config_class = new class {
+ function __construct()
+ {
+ }
+};
+
+// --- Global Functions --------------------------------------------------
+function config($key, $default_value) {}
+
+// --- Definitions --------------------------------------------------
+define("AUTHORIZED_COMMANDS", [
+    " " => "",
+    "add" => "",
+    "update" => "",
+    "delete" => "",
+    "mark-in-progress" => "",
+    "mark-done" => "",
+    "list" => "",
+    "list done" => "",
+    "list todo" => "",
+    "list in-progress" => "",
+]);
+
+// --- OOP --------------------------------------------------
+class Console
+{
+	use Traits\HasTrait;
+
+	private string $base_path;
+	private string $config_path;
+
+
+		//public $output_file = (string) '';
+
+	    /** command has description, has subcommands that calls a function */
+	    //public $commands_files = (array) [];
+
+
+		/**
+		 * @return void
+		 */
+		function handle(string $base_path):void
+		{
+			$this->base_path = $base_path;
+			$this->cmd_response();
+		}
+
+    // --- Initialization --------------------------------------------------
+
+    /**
+    * @param string $command
+    * @return string
+    */
+    private function cmd_not_found($command): string
+    {
+        return "Unknown command: $command\n";
+    }
+    //command has description and argument that calls a function or not and each argument has a description
+    /** @return void */
+    function cmd_response():void
+    {
+        global $argc;
+        global $argv;
+        if (empty($argv[1])) {
+            echo "Show Help";
+        } else {
+            echo $argv[1];
+        }
+    }
+}
+
+// --- Result --------------------------------------------------
+return new Console();
